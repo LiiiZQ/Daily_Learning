96ba6a0a62312d5b060012aebb856b3c23073dfd
子青Li <45389890+LiiiZQ@users.noreply.github.com>
2020-07-15 23:22:29 +0800

    Update pro1.java

--- 2020JUL/pro1.java
+++ 2020JUL/pro1.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+// 两数之和
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int len = nums.length;
-        
-        /*
+        int[] data = new int[2];
+        /*(n^2-n)/2
         for(int i = 0; i < len-1; i++){
             for(int j = i+1; j < len; j++){
                 if(nums[i]+nums[j] == target){
@@ -10,8 +13,18 @@ class Solution {
                 }
             }
         }
-        
-        return null;
-        */
+        return null;*/
+        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+        for(int i = 0; i < len; i++){
+            if(hash.containsKey(nums[i])){
+                data[0] = i;
+                data[1] = hash.get(nums[i]);
+                return data;
+            }else{
+                int index = target - nums[i];
+                hash.put(index, i);
+            }
+        }
+        return data;
     }
 }
